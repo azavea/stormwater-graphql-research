54bca7391ea8a1b03990e2fbdd09e0fcc6f4b3d0
Kelly Innes <kinnes@gmail.com>
2019-01-20 20:31:47 -0500

    Configure Apollo

--- mobile/App.js
+++ mobile/App.js
@@ -1,21 +1,19 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import ApolloClient from 'apollo-boost';
+import { ApolloProvider } from 'react-apollo';
 
-export default class App extends React.Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-      </View>
-    );
-  }
-}
+import Main from './components/Main';
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+
+const client = new ApolloClient({
+    uri: 'http://localhost:9991/',
 });
+
+
+export default function App() {
+    return (
+        <ApolloProvider client={client}>
+            <Main />
+        </ApolloProvider>
+    );
+}
