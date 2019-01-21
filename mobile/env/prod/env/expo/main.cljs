c14253a79349e5865397567573ab45246ab2fcc1
Kelly Innes <kinnes@gmail.com>
2019-01-20 19:44:03 -0500

    Replace CLJS Expo with JS Expo app

--- mobile/env/prod/env/expo/main.cljs
+++
@@ -1,4 +0,0 @@
-(ns env.expo.main
-  (:require [cljs-rn-sandbox.core :as core]))
-
-(core/init)
--- mobile/src/mobile_graphql_client/constants.cljs
+++
@@ -1,8 +0,0 @@
-(ns mobile-graphql-client.constants)
-
-(defonce map-screen "map-screen")
-(defonce settings-screen "settings-screen")
-(defonce initial-map-region {:latitude 39.9524
-                             :longitude -75.1636
-                             :latitudeDelta 0.0922
-                             :longitudeDelta 0.0421})
