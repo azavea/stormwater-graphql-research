c2c00ee02a026778b14f93528edbd23a93318b81
Kelly Innes <kinnes@azavea.com>
2019-02-01 15:09:09 -0500

    Revert "Merge pull request #29 from azavea/ki/upgrade-to-react-16-7-alpha"
    This reverts commit 328867c49159608e22c1d1fe9d16006651560b46, reversing
    changes made to 23f92609190f0c8725da8df27f97abd972aec74e.

--- web/src/components/ReactLeafletMap.jsx
+++ web/src/components/ReactLeafletMap.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { Component } from 'react';
 import { Map } from 'react-leaflet';
 
 import {
@@ -16,69 +16,113 @@ import ParcelPolygon from './ParcelPolygon';
 
 const REACT_LEAFLET_MAP_ID = 'react-leaflet-map';
 
-export default function ReactLeafletMap() {
-    const [selectPointMode, setSelectPointMode] = useState(false);
-    const [fetching, setFetching] = useState(false);
-    const [queryMode, setQueryMode] = useState(queryModeEnum.RWD);
-    const [lat, setLat] = useState(null);
-    const [lng, setLng] = useState(null);
+export default class ReactLeafletMap extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            lat: null,
+            lng: null,
+            selectPointMode: false,
+            fetching: false,
+            queryMode: queryModeEnum.RWD,
+        };
 
-    const stopFetching = () => setFetching(false);
-    const toggleSelectPointMode = () => setSelectPointMode(!selectPointMode);
+        this.handleMapClick = this.handleMapClick.bind(this);
+        this.toggleSelectPointMode = this.toggleSelectPointMode.bind(this);
+        this.stopFetching = this.stopFetching.bind(this);
+        this.toggleRWDParcelMode = this.toggleRWDParcelMode.bind(this);
+    }
 
-    const toggleRWDParcelMode = queryMode === queryModeEnum.RWD
-        ? () => setQueryMode(queryModeEnum.Parcel)
-        : () => setQueryMode(queryModeEnum.RWD);
+    handleMapClick({ latlng }) {
+        return this.state.selectPointMode
+            ? this.setState(state => ({
+                ...state,
+                ...latlng,
+                selectPointMode: false,
+                fetching: true,
+            }))
+            : null;
+    }
 
-    const handleMapClick = ({ latlng }) => {
-        if (!selectPointMode) {
-            return null;
-        }
-
-        setFetching(true);
-        setSelectPointMode(false);
-        setLat(latlng.lat);
-        setLng(latlng.lng);
-
-        return null;
-    };
-
-    const polygon = queryMode === queryModeEnum.RWD
-        ? (
-            <RWDPolygon
-                lat={lat}
-                lng={lng}
-                stopFetching={stopFetching}
-            />)
-        : (
-            <ParcelPolygon
-                lat={lat}
-                lng={lng}
-                stopFetching={stopFetching}
-            />
+    toggleSelectPointMode() {
+        return this.setState(
+            state => ({
+                ...state,
+                selectPointMode: !state.selectPointMode,
+            }),
+            () => {
+                document
+                    .getElementById(REACT_LEAFLET_MAP_ID)
+                    .style
+                    .cursor = this.state.selectPointMode
+                        ? 'pointer'
+                        : null;
+            },
         );
+    }
 
-    return (
-        <Map
-            center={cityHallCoordinates}
-            zoom={initialMapZoom}
-            id={REACT_LEAFLET_MAP_ID}
-            onClick={handleMapClick}
-        >
-            <ReactLeafletEsriTiledMapLayer />
-            <ToggleRWDParcelModeControl
-                loading={fetching}
-                position={controlPositionsEnum.topleft}
-                mode={queryMode}
-                toggleRWDParcelMode={toggleRWDParcelMode}
-            />
-            <SelectPointControl
-                loading={fetching}
-                position={controlPositionsEnum.topleft}
-                selectPointMode={selectPointMode}
-                toggleSelectPointMode={toggleSelectPointMode}
-            />
-            {polygon}
-        </Map>
-    );
+    toggleRWDParcelMode() {
+        return this.setState(state => ({
+            ...state,
+            queryMode: state.queryMode === queryModeEnum.RWD
+                ? queryModeEnum.Parcel
+                : queryModeEnum.RWD,
+        }));
+    }
+
+    stopFetching() {
+        return this.setState(state => ({
+            ...state,
+            fetching: false,
+        }));
+    }
+
+    render() {
+        const {
+            lat,
+            lng,
+            selectPointMode,
+            fetching,
+            queryMode,
+        } = this.state;
+
+        const polygon = queryMode === queryModeEnum.RWD
+            ? (
+                <RWDPolygon
+                    lat={lat}
+                    lng={lng}
+                    stopFetching={this.stopFetching}
+                />)
+            : (
+                <ParcelPolygon
+                    lat={lat}
+                    lng={lng}
+                    stopFetching={this.stopFetching}
+                />
+            );
+
+        return (
+            <Map
+                center={cityHallCoordinates}
+                zoom={initialMapZoom}
+                id={REACT_LEAFLET_MAP_ID}
+                onClick={this.handleMapClick}
+            >
+                <ReactLeafletEsriTiledMapLayer />
+                <ToggleRWDParcelModeControl
+                    loading={fetching}
+                    position={controlPositionsEnum.topleft}
+                    mode={queryMode}
+                    toggleRWDParcelMode={this.toggleRWDParcelMode}
+                />
+                <SelectPointControl
+                    loading={fetching}
+                    position={controlPositionsEnum.topleft}
+                    selectPointMode={selectPointMode}
+                    toggleSelectPointMode={this.toggleSelectPointMode}
+                />
+                {polygon}
+            </Map>
+        );
+    }
 }
